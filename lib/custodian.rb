34ea63fe3d9e3961391c4efc3ac50646574ea60a
Tom Armitage <tom@infovore.org>
2009-03-06 12:10:48 +0000

    Bumping version number, innit.

--- lib/custodian.rb
+++ lib/custodian.rb
@@ -2,7 +2,7 @@ $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
 module Custodian
-  VERSION = '0.0.2'
+  VERSION = '0.0.3'
   
   def self.proxy_addr
     @@proxy_addr
